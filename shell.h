df6953941a1f430c23fc58670f102bb5f4225f1b
Cheto02 <fredycheto@gmail.com>
2024-01-10 13:31:37 -0800

    betty checks on updates

--- shell.h
+++ shell.h
@@ -19,16 +19,8 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-
-
 void print_out(const char *msg);
 void shell_prompt(void);
 void mread_command(char *command, size_t size);
-<<<<<<< HEAD
 void executorto_command(const char *command);
-=======
-
->>>>>>> aa4ac926a177c91cb1dded1e36afe8b5f72cd861
-
-
 #endif /* SHELL_H */
